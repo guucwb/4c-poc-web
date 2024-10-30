11602e9f8aec5b9b57acae68b2560503941e8a02
André Silva <ansilva@twilio.com>
2024-10-30 12:15:16 -0300

    4C

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { isEmpty } from 'lodash';
-import axios from 'axios';  
+//import axios from 'axios';  
 
 import { useUID } from '@twilio-paste/core/dist/uid-library';
 import { Theme } from '@twilio-paste/core/theme';
@@ -33,18 +33,19 @@ function App() {
 
   useEffect(() => {
     if (phoneNumber && phoneNumber !== "") {
-      let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://paschoalotto-7229.twil.io/getData?key=2XT8P3Y7VL&phoneNumber=' + phoneNumber,
-        headers: { 
-          'Content-Type': 'application/json', 
-        }
-      };
+      setData({ "data": {"debt":{"currentValue":"R$ 8.253,56","creditor":"Banco Itaú S/A","originalDate":"25/03/2024","debtType":"Cartão de Crédito","originalValue":"R$ 3.542,77","debtStatus":"Negativada"},"registration":{"dob":"04/07/1975","cityState":"Guarulhos / SP","cpf":"12345678909","rg":"24034518-1","phoneNumber":"(11) 99280-8313","address":"Rua Áustria, 168 - Parque das Nações","fullName":"André Silva"}}});
+      // let config = {
+      //   method: 'post',
+      //   maxBodyLength: Infinity,
+      //   url: 'https://paschoalotto-7229.twil.io/getData?key=2XT8P3Y7VL&phoneNumber=' + phoneNumber,
+      //   headers: { 
+      //     'Content-Type': 'application/json', 
+      //   }
+      // };
 
-      axios.request(config)
-        .then((response) => setData(response.data))
-        .catch((_err) => { console.log('phone number not found') });
+      // axios.request(config)
+      //   .then((response) => setData(response.data))
+      //   .catch((_err) => { console.log('phone number not found') });
     }
   }, [phoneNumber]);
 
@@ -80,7 +81,7 @@ function App() {
             </>
           }
           { noData && 
-            <img className="main-logo" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReBkJcmAgPCYXjZWwzk-EGE_wKWO-MIIKEKw&s' alt='logo' />
+            <img className="main-logo" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDJa7oBBWKZgiLDr2fBLN2Be5CMLTLCUnRag&s' alt='logo' />
           }
         </Box>
       </Theme.Provider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,8 @@ function Header() {
     return (
         <>
             <Box paddingBottom="space60" display="flex">
-                <a href="https://www.pagoufacil.com.br/" style={{alignItems: 'center', display: 'flex', marginLeft: 'auto'}}>
-                    <img src="https://www.pagoufacil.com.br/wp-content/themes/pagoufacil/img/logo-pagoufacil-lp.svg" alt="Pagou Facil" /> 
-                    <img src="https://www.pagoufacil.com.br/wp-content/themes/pagoufacil/img/risco.png" alt="" style={{ margin: '0 8px'}} /> 
-                    <img src="https://www.pagoufacil.com.br/wp-content/themes/pagoufacil/img/logo-paschoalotto-lp.png" alt="Paschoalotto" />
+                <a href="https://www.4cdigital.com.br/" style={{alignItems: 'center', display: 'flex', marginLeft: 'auto'}}>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDJa7oBBWKZgiLDr2fBLN2Be5CMLTLCUnRag&s" alt="4C Digital" style={{ height: '75px'}} /> 
                 </a>
             </Box>
         </>
